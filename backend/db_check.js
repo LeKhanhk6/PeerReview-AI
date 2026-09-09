ef4928105c147ec3b041e9133e1b56d7b3530f70
LeQuocKhanh <doituyentin9a1@gmail.com>
2026-09-09 21:40:35 +0700

    chore(cleanup): remove unused scratch scripts and legacy tests directory

--- backend/db_check.js
+++
@@ -1,7 +0,0 @@
-import pool from './src/config/db.js';
-
-(async () => {
-    const res = await pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'groups'");
-    console.log(res.rows);
-    process.exit(0);
-})();
